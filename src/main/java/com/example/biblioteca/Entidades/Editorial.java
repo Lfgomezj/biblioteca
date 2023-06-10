763007b9503279f84d16f7d7918300f59d0b1543
Luisa <luisa.gj00@gmail.com>
2023-06-10 16:04:14 -0500

    se crea repositorio autor

--- src/main/java/com/example/biblioteca/Entidades/Editorial.java
+++ src/main/java/com/example/biblioteca/Entidades/Editorial.java
@@ -1,4 +1,53 @@
 package com.example.biblioteca.Entidades;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table (name="editorial")
+
 public class Editorial {
+
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="nombre",nullable = false, length =50 )
+    private String nombre;
+    @Column(name="descripcion",nullable = false, length =50 )
+    private String descripcion;
+
+    public Editorial(Integer id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public Editorial() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- src/main/java/com/example/biblioteca/Repositorios/RepositorioAutor.java
+++ src/main/java/com/example/biblioteca/Repositorios/RepositorioAutor.java
@@ -1,2 +1,9 @@
-package com.example.biblioteca.Repositorios;public interface RepositorioAutor {
+package com.example.biblioteca.Repositorios;
+
+import com.example.biblioteca.Entidades.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositorioAutor extends JpaRepository<Autor,Integer> {
 }
